16a03152cafbe1960e979cd9cde84f763b03b4fd
Hugo Viana <hugo.vss@outlook.com>
2025-12-12 17:29:17 -0300

    Modifiquei as propriedades do Smartphone para usar setters privados.
    Changed properties to have private setters for encapsulation.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,9 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smarpnhone
     {
         public string Numero { get; set; }
-        public string Modelo { get; set; }
-        public string IMEI { get; set; }
-        public int Memoria { get; set; }
+        public string Modelo { get; private set; }
+        public string IMEI { get; private set; }
+        public int Memoria { get; private set; }
 
         public Smarpnhone(string numero, string modelo, string imei, int memoria)
         {
