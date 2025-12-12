1532e159f395ef09c969347efbdc552234d8b17b
Hugo Viana <hugo.vss@outlook.com>
2025-12-12 17:10:36 -0300

    Adiciona funcionalidade para escolher entre Nokia e iPhone para usar.
    Implement user interaction for selecting and using Nokia or iPhone.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("6123456789", "Nokia 12345", "IMEI1234567890", 256);
+Iphone iphone = new Iphone("6198765432", "iPhone 13", "IMEI0987654321", 512);
+
+Console.WriteLine("Qual celular você quer usar? (Nokia/Iphone): ");
+string celular = Console.ReadLine();
+
+if (celular == "Nokia")
+{
+    Console.WriteLine("Usando o nokia");
+
+    nokia.Ligar();
+    nokia.InstalarAplicativo("WhatsApp");
+    nokia.ReceberLigacao();
+}
+
+else
+{
+    Console.WriteLine("Usando o iphone");
+    iphone.Ligar();
+    iphone.InstalarAplicativo("Instagram");
+    iphone.ReceberLigacao();
+}
